ef5b67fbb91771e5f83d91c6f7e4921d824623d9
Russell Hart <russellbot@gmail.com>
2021-01-29 15:55:20 -0500

    get category by id

--- routers/categories.js
+++ routers/categories.js
@@ -8,7 +8,17 @@ router.get(`/`, async (req, res) => {
     if(!categoryList) {
         res.status(500).json({success: false})
     }
-    res.send(categoryList);
+    res.status(200).send(categoryList);
+})
+
+router.get('/:id', async(req,res) => {
+    const category = await Category.findById(req.params.id);
+
+    if(!category) {
+        return res.status(500).send('the category with the given id was not found')
+    }    
+
+    res.status(200).send(category);
 })
 
 router.post('/', async (req, res) => {
@@ -19,8 +29,9 @@ router.post('/', async (req, res) => {
     })
     category = await category.save();
 
-    if(!category)
-    return res.status(404).send('the category cannot be created')
+    if(!category) {
+        return res.status(404).send('the category cannot be created')
+    }    
 
     res.send(category);
 })
