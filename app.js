7f952ef03540f7ad1f940b1fee434d468754f57f
Russell Hart <russellbot@gmail.com>
2021-01-29 13:31:17 -0500

    cors

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors());
 
 // middleware
 app.use(bodyParser.json());
