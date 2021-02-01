09ef5e3ae01ec59933449321d9aa8e851f00cb5c
Russell Hart <russellbot@gmail.com>
2021-02-01 12:31:00 -0500

    get user count and delete user

--- routers/users.js
+++ routers/users.js
@@ -92,4 +92,27 @@ router.post('/register', async (req, res) => {
     res.send(user);
 })
 
+router.get(`/get/count`, async (req, res) => {
+    const userCount = await User.countDocuments((count) => count);
+
+    if(!userCount) {
+        res.status(500).json({success: false})
+    }
+    res.send({
+        userCount: userCount
+    });
+})
+
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id).then(user => {
+        if(user) {
+            return res.status(200).json({success: true, message: 'the user is deleted'})
+        } else {
+            return res.status(404).json({success: false, message: 'user not found'})
+        }
+    }).catch(err => {
+        return res.status(400).json({success: false, error: err})
+    })
+})
+
 module.exports = router;
