137592d5c9ecbd8c769aa7ece5e0b611f2fbb64e
Russell Hart <russellbot@gmail.com>
2021-02-01 16:29:14 -0500

    post order and create order items

--- routers/orders.js
+++ routers/orders.js
@@ -1,4 +1,5 @@
 const { Order } = require('../models/order')
+const { OrderItem } = require('../models/order-item')
 const express = require('express');
 const router = express.Router();
 
@@ -9,6 +10,39 @@ router.get(`/`, async (req, res) => {
         res.status(500).json({success: false})
     }
     res.send(orderList);
+})
+
+router.post('/', async (req, res) => {
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async orderItem => {
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id;
+    }))
+    const orderItemsIdsResolved = await orderItemsIds;
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.city,
+        country: req.body.country,
+        phone: req.body.phone,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
+    })
+    order = await order.save();
+
+    if(!order) {
+        return res.status(404).send('the order cannot be created')
+    }    
+
+    res.send(order);
 })
 
 module.exports = router;
