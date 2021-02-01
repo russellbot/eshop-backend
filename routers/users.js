3eccbce370987a03af118e1f21c7a5adf849d517
Russell Hart <russellbot@gmail.com>
2021-02-01 12:01:29 -0500

    excluding paths from jwt authentication

--- routers/users.js
+++ routers/users.js
@@ -69,4 +69,26 @@ router.post('/login', async (req,res) => {
     return res.status(200).send(user);
 })
 
+router.post('/register', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user) {
+        return res.status(400).send('the user cannot be created')
+    }    
+
+    res.send(user);
+})
+
 module.exports = router;
