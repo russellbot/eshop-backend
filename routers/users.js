ba848b676163b38ad0640a04166f90d6fa7aea44
Russell Hart <russellbot@gmail.com>
2021-02-01 11:16:17 -0500

    user api creating a JWT

--- routers/users.js
+++ routers/users.js
@@ -2,14 +2,25 @@ const { User } = require('../models/user')
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.get(`/`, async (req, res) => {
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash');
 
     if(!userList) {
         res.status(500).json({success: false})
     }
     res.send(userList);
+})
+
+router.get('/:id', async(req,res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if(!user) {
+        return res.status(500).send('the user with the given id was not found')
+    }    
+
+    res.status(200).send(user);
 })
 
 router.post('/', async (req, res) => {
@@ -32,6 +43,30 @@ router.post('/', async (req, res) => {
     }    
 
     res.send(user);
+})
+
+router.post('/login', async (req,res) => {
+    const user = await User.findOne({email: req.body.email})
+    const secret = process.env.jwt_secret;
+
+    if (!user) {
+        return res.status(400).send('The user not found');
+    }
+
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+            userId: user.id
+            },
+            secret,
+            {expiresIn: '1d'}
+        )
+        res.status(200).send({user: user.email, token: token})
+    } else {
+        res.status(400).send('password is wrong')
+    }
+
+    return res.status(200).send(user);
 })
 
 module.exports = router;
