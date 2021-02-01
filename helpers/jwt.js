750e4f9d9179c24c2fdd016b17ba38d27ac0e933
Russell Hart <russellbot@gmail.com>
2021-02-01 12:27:01 -0500

    restrict some api endpoints to users

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,15 +5,27 @@ function authJwt() {
     const api = process.env.API_URL;
     return expressJwt({
         secret,
-        algorithms: ['HS256']
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
     }).unless({
         path: [
+            // paths to exclude from needing authorization token
+            // regex to get all GET product endpoints
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             `${api}/users/login`,
             `${api}/users/register`
         ]
     })
+}
+
+// this function lets admin access everything
+async function isRevoked(req, payload, done) {
+    if(!payload.isAdmin) {
+        done(null, true)
+    }
+
+    done();
 }
 
 module.exports = authJwt;
