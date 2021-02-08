cac801d5c7176d84b5f6b232eab363393a1f9b54
Russell Hart <russellbot@gmail.com>
2021-02-08 14:16:20 -0500

    added users to whitelist

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,9 +11,10 @@ function authJwt() {
         path: [
             // paths to exclude from needing authorization token
             // regex to get all GET product endpoints and to public uploads for pictures
-            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            { url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             `${api}/users/login`,
             `${api}/users/register`
         ]
