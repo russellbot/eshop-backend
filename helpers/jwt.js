b39b863a7854e7c201a0a0f88e0b6dfba1dcbcde
Russell Hart <russellbot@gmail.com>
2021-02-08 11:19:59 -0500

    jwt secret

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,7 +1,7 @@
 const expressJwt = require('express-jwt');
 
 function authJwt() {
-    const secret = process.env.jwt_secret;
+    const secret = process.env.JWT_SECRET;
     const api = process.env.API_URL;
     return expressJwt({
         secret,
